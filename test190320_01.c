7a7f07ab55d924c5fee972faf36914489d71b3b9
liwenlong <1798946643@qq.com>
2019-03-21 09:31:33 +0800

    新增递归方式和非递归方式实现大数取余

--- test190320_01.c
+++ test190320_01.c
@@ -5,15 +5,14 @@
 输入正整数m， 输出满足如下条件的最小正整数a：
 对任意0<i,j<m且i！=j，有a^i%m != a^j%m
 
-思路：从1到m-1遍历，找出第一个满足条件的a，如都不满足，则返回-1。
-考试的时候没有考虑int溢出的问题，导致只有通过了40%的测试用例。
+思路：大数取余法，a*b%n=(a%n*b%n)%n防止溢出 
 **/
 
 #include<stdio.h>
 
 #define N 100000
 
-// 计算x^y%m，运用公式a*b%n=(a%n*b%n)%n防止int溢出
+// 计算x^y%m 递归实现 
 int getM(int x, int y, int m) {
 	int temp1, temp2;
 	int i;
@@ -23,6 +22,17 @@ int getM(int x, int y, int m) {
 	temp1 = getM(x, y/2, m);
 	temp2 = getM(x, y-y/2, m);
 	return (temp1*temp2)%m;
+}
+
+// 计算x^y%m 非递归实现 
+int getM2(int x, int y, int m) {
+	int temp = 1;
+	int i;
+	for(i=0; i<y; i++){
+		temp *= x;
+		temp %= m;
+	} 
+	return temp;
 }
 
 int check(int res[], int m) {
@@ -46,7 +56,7 @@ int main() {
 
 	for(a=2; a<m; a++) {
 		for(i=1; i<m; i++) {
-			res[i] = getM(a, i, m);
+			res[i] = getM2(a, i, m);
 		}
 		if(check(res, m)==1) {
 			ans = a;
